b4c5f9ba69b423a0912842a9932c67498e437dd3
light-planck <seseifunc16@gmail.com>
2025-10-19 19:31:40 +0900

    chap2

---
+++ src/main.rb
@@ -0,0 +1,12 @@
+require_relative 'gear'
+require_relative 'wheel'
+
+def main
+  wheel = Wheel.new(26, 1.5)
+  puts wheel.circumference
+
+  puts Gear.new(52, 11, wheel).gear_inches
+  puts Gear.new(52, 11).ratio
+end
+
+main
